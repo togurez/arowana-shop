a49ca72a77a9642304f0f611d69450f64052db3c
Alex Yazkov <yazkov@adobe.com>
2022-12-27 17:24:34 -0800

    firebase support

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeJiTTV7LMYWOFMAiWV2hv38eZNiExRqQ",
+    authDomain: "arowana-shop-db.firebaseapp.com",
+    projectId: "arowana-shop-db",
+    storageBucket: "arowana-shop-db.appspot.com",
+    messagingSenderId: "741400409402",
+    appId: "1:741400409402:web:a0031f683608fa80f88c03"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {displayName, email, createdAt});
+        } catch(error) {
+            console.log("error creating the user", error.message);
+        }
+    }
+    return userDocRef;
+}
